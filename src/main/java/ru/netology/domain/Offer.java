ad3090052bcc66ebac0e3f3d243cf3909dffe33d
Ellijah Ilyin <welder265@qmail.com>
2021-01-13 18:58:56 +0300

    Added all of stuff

--- src/main/java/ru/netology/domain/Offer.java
+++ src/main/java/ru/netology/domain/Offer.java
@@ -37,18 +37,6 @@ public class Offer implements Comparable<Offer> {
         return id;
     }
 
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public int getAmount() {
-        return amount;
-    }
-
-    public void setAmount(int amount) {
-        this.amount = amount;
-    }
-
     public String getDepartureAirport() {
         return departureAirport;
     }
